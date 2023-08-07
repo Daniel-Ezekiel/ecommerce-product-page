9d4a631da1f3773610415b8434c9fbc01dbbcbcb
Daniel Ezekiel <dbezekiel77@gmail.com>
2023-08-07 11:36:42 +0100

    feat: mobile image carousel functional

--- js/main.js
+++ js/main.js
@@ -12,3 +12,33 @@ btnOpenMenu.addEventListener("click", () => {
 btnCloseMenu.addEventListener("click", () => {
   mobileNav.classList.remove("active");
 });
+
+// Mobile Product Images carousel
+let count = 1;
+
+const mobileFigElement = document.querySelector("figure.mobile");
+const productImg = document.querySelector(".mobile-product-image");
+const btnPrevImage = document.querySelector("#btn-previous");
+const btnNextImage = document.querySelector("#btn-next");
+
+function displayProductImage() {
+  const imgSrc = `./images/image-product-${count}.jpg`;
+  productImg.src = imgSrc;
+}
+displayProductImage(count);
+
+btnPrevImage.addEventListener("click", (e) => {
+  count--;
+  if (count < 1) {
+    count = 4;
+  }
+  displayProductImage();
+});
+
+btnNextImage.addEventListener("click", (e) => {
+  count++;
+  if (count > 4) {
+    count = 1;
+  }
+  displayProductImage();
+});
